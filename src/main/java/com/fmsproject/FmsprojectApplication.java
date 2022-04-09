ad79bad73e02d1713318f0f973411bede2e1d2e6
Akash Bonde <akashbonde@live.com>
2022-04-09 15:37:26 +0530

    First commit

---
+++ src/main/java/com/fmsproject/FmsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.fmsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmsproject/FmsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
