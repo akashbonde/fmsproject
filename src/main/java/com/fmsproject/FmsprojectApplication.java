7dd8826bb3b38ae807757544db36ad9e9a218103
Akash Bonde <akashbonde@live.com>
2022-04-17 13:46:41 +0530

    Addded swagger documentation

--- src/main/java/com/fmsproject/FmsprojectApplication.java
+++ src/main/java/com/fmsproject/FmsprojectApplication.java
@@ -2,9 +2,20 @@ package com.fmsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@ComponentScan(basePackages = {"com.fmsproject"})
+@EnableSwagger2
 @SpringBootApplication
+@Configuration("com.fmsproject.controller")
 @PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class FmsprojectApplication {
 
@@ -12,6 +23,12 @@ public class FmsprojectApplication {
 		SpringApplication.run(FmsprojectApplication.class, args);
 		System.out.println("The application is now running!!");
 		
+	}
+	
+	//The URL for the swagger is : http:localhost:8086/swagger-ui.html 
+	@Bean
+	public Docket fmsprojectapi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.fmsproject")).build();
 	}
 
 }
