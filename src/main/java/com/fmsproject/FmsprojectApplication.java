0406bec6be8b600f8b371965862190f04bb47721
Akash Bonde <akashbonde@live.com>
2022-04-18 16:56:24 +0530

    Added resource handlers

--- src/main/java/com/fmsproject/FmsprojectApplication.java
+++ src/main/java/com/fmsproject/FmsprojectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.builders.PathSelectors;
@@ -45,4 +46,11 @@ public class FmsprojectApplication extends WebMvcConfigurerAdapter{
 	            "urn:tos", "akashbonde@live.com", "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
 	}
 
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");  
+       registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
